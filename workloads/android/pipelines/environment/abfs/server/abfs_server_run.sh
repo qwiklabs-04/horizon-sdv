718f5a5c28654608ae7f863c38523c1d512fa94c
David Smith <dave.m.smith@accenture.com>
2025-07-23 13:46:33 +0100

    TAA-929: Failed os login due to excessive SSH keys

--- workloads/android/pipelines/environment/abfs/server/abfs_server_run.sh
+++ workloads/android/pipelines/environment/abfs/server/abfs_server_run.sh
@@ -22,6 +22,15 @@ module "abfs-server" {
 EOL
 }
 
+# Clean old SSH keys
+function abfs_clean_ssh_keys() {
+  # https://cloud.google.com/compute/docs/troubleshooting/troubleshoot-os-login#invalid_argument
+  echo -e "Remove old SSH keys"
+  gcloud compute os-login describe-profile | \
+      awk -v username="$(whoami)" '/fingerprint:/{f=$2} $0 ~ username && /instance/{print f}' | \
+      xargs -I {} gcloud compute os-login ssh-keys remove --key={} || true
+}
+
 function abfs_server_run() {
   echo "ABFS Server Run"
 
@@ -70,6 +79,7 @@ function abfs_server_update_schema() {
   rm -rf "${REPO_DIRECTORY}"
 }
 
+abfs_clean_ssh_keys
 abfs_override_tf
 abfs_server_run
 abfs_server_update_schema

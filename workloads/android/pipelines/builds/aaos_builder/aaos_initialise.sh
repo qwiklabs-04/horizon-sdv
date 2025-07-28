0d2089144167405a90aa759a47e09405032e6f98
David Smith <dave.m.smith@accenture.com>
2025-07-28 17:21:02 +0100

    TAA-929: Avoid dumping repo list to console

--- workloads/android/pipelines/builds/aaos_builder/aaos_initialise.sh
+++ workloads/android/pipelines/builds/aaos_builder/aaos_initialise.sh
@@ -188,7 +188,8 @@ function abfs_install() {
     echo "abfs_install."
     sudo apt update -y
     declare -r abfs_artifacts="${ORIG_WORKSPACE}"/abfs_repository_list.txt
-    gcloud artifacts files list --project=abfs-binaries --location=us --repository="${ABFS_REPOSITORY}" | tee -a "${abfs_artifacts}"
+    rm -f "${abfs_artifacts}"
+    gcloud artifacts files list --project=abfs-binaries --location=us --repository="${ABFS_REPOSITORY}" > "${abfs_artifacts}" 2>&1
     # shellcheck disable=SC2002
     cat "${abfs_artifacts}" | grep -e "pool/abfs.*client_${ABFS_CLIENT_VERSION}" -e "pool/casfs-kmod-$(uname -r)_${ABFS_VERSION}" | awk '{print $1}' | while read -r a; do gcloud artifacts files download --project=abfs-binaries --location=us --repository="${ABFS_REPOSITORY}" --destination=. "${a}"; done
     CMD="find . -maxdepth 1 -type f -name \"pool*\" -exec sudo apt install \"./{}\" \\;"

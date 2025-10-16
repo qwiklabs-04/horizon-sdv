a547b9ae7d9a645c5f80f9a8530e3bbf6663dc58
David Smith <dave.m.smith@accenture.com>
2025-10-16 15:39:43 +0100

    TAA-1148: Update CF to latest stable (v1.27.0)

--- workloads/android/pipelines/tests/cts_execution/groovy/job.groovy
+++ workloads/android/pipelines/tests/cts_execution/groovy/job.groovy
@@ -44,9 +44,9 @@ href="https://source.android.com/docs/compatibility/cts/downloads" target="_blan
       description('''<p>The Jenkins GCE Clouds label for the Cuttlefish instance template, e.g.<br/></p>
         <ul>
           <li>cuttlefish-vm-main</li>
-          <li>cuttlefish-vm-v1250</li>
+          <li>cuttlefish-vm-v1270</li>
           <li>cuttlefish-vm-main-arm64</li>
-          <li>cuttlefish-vm-v1250-arm64</li>
+          <li>cuttlefish-vm-v1270-arm64</li>
         </ul>''')
       trim(true)
     }

d6bb411d5da058d755bebee2a8e69b1e8285992a
David Smith <dave.m.smith@accenture.com>
2025-10-23 09:27:05 +0100

    TAA-1182: OpenBSW clang artifacts
    Generic path for convenience.

--- workloads/openbsw/pipelines/builds/bsw_builder/bsw_environment.sh
+++ workloads/openbsw/pipelines/builds/bsw_builder/bsw_environment.sh
@@ -182,16 +182,13 @@ fi
 
 if ${BUILD_NXP_S32K148}; then
     OPENBSW_ARTIFACT_LIST+=(
-        "${OPENBSW_GIT_DIR}/artifacts/s32k148-gcc"
-        "${OPENBSW_GIT_DIR}/artifacts/s32k148-clang"
+        "${OPENBSW_GIT_DIR}/artifacts/s32k148"
     )
     POST_BUILD_COMMANDS+=(
-        "mkdir -p artifacts/s32k148-gcc"
-        "mkdir -p artifacts/s32k148-clang"
-        "cp -f ${NXP_S32K148_ARTIFACT} artifacts/s32k148-gcc || true"
-        "cp -f ${NXP_S32K148_ARTIFACT} artifacts/s32k148-clang || true"
-        "cp -f build/s32k148-gcc/application.map artifacts/s32k148-gcc || true"
-        "cp -f build/s32k148-clang/application.map artifacts/s32k148-clang || true"
+        "mkdir -p artifacts/s32k148"
+        "cp -f ${NXP_S32K148_ARTIFACT} artifacts/s32k148 || true"
+        "cp -f build/s32k148-gcc/application.map artifacts/s32k148 || true"
+        "cp -f build/s32k148-clang/application.map artifacts/s32k148 || true"
     )
 fi
 

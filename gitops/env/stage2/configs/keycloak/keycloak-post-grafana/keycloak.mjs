74b2a80fc10ca696e79c31c383079322d873d96a
lukaszdom <lukasz.domke@accenture.com>
2025-10-28 10:09:46 +0100

    TAA 749 postjob keycloak grafana (#673)
    * rebase to new branch1
    
    * rebase to new branch2
    
    * rebase to new branch3
    
    * sa test and domain in gateway
    
    * ignore rules test1
    
    * ignore sync rules test1 fix
    
    * ignore sync rules test2
    
    * ignore sync rules test3
    
    * ignore sync rules test4
    
    * ignore sync rules test5
    
    * ignore sync rules test 6
    
    * ignore sync rules test 7
    
    * ignore sync rules test 7 fix
    
    * ignore sync rules test 8
    
    * back to ignore sync rules test 7
    
    * ignore sync rules test 9
    
    * ignore sync rules test 10
    
    * ignore sync rules test 11
    
    * conf1
    
    * 2 postjob init 2
    
    * 2 postjob init - added grafana post
    
    * 2 postjob init - added grafana post 2
    
    * 2 postjob 2 fix1
    
    * 2 postjob 2 fix2
    
    * fixes1
    
    * pass changed1
    
    * pass from secret key1
    
    * pass test2
    
    * pass test3
    
    * pass test 4
    
    * pass test 5
    
    * pass removed1
    
    * username 1
    
    * username 2
    
    * debug added
    
    * update for PR
    
    * debug 0
    
    * group names fix
    
    * namespace gmp-system fix
    
    * operator conf and monitoring namespace in collector
    
    * sa promethues test1
    
    * added pod monitoring1 kubestate
    
    * sa collector-sa in prom1 and operator config disabled
    
    * dashb 15661 v2 test1
    
    * dashb 15661 v1 back
    
    * removed OperatorConfig not needed1
    
    * review fixes 1
    
    * review fixes 2
    
    * review fixes 2
    
    * review fixes 3
    
    * Update prometheus-ui-frontend.yaml
    
    * TAA-1133 imp1
    
    * TAA-1133 imp2
    
    * TAA-1133 test1
    
    * TAA-1133 test2
    
    * TAA-1133 test3
    
    * TAA-1133 test4
    
    * TAA-1133 test5
    
    * TAA-1133 test6 target rev
    
    * TAA-1133 test6 target rev1
    
    * TAA-1133 test7 projID
    
    * TAA-1133 test8 target rev1
    
    * TAA-1133 test8 target rev2
    
    * TAA-1133 test8 target rev3
    
    * TAA-1133 test8 target rev4
    
    * app rename1
    
    * replica 1
    
    * values upd1
    
    * frontend 1 replica
    
    * prometheus-frontend upate in grafana
    
    * prometheus-frontend replica test1
    
    * values1
    
    * fix1
    
    * target rev test1
    
    * values2
    
    * sync-wave removed on apps level1
    
    * affinity removed
    
    * affinity removed1
    
    * affinity removed2
    
    * improvements test1
    
    * improvements test2
    
    * improvements test3
    
    * improvements test4
    
    * improvements test 5
    
    * improvements test 6
    
    * improvements test 7
    
    * improvements test 8
    
    * improvements test 9
    
    * improvements test 10
    
    * improvements test 11
    
    * improvements test 12 labels on
    
    * improvements test 13 horizonatl autoscaller removed
    
    * improvements test 14 horizonatl autoscaller on
    
    * improvements test 15 label on
    
    * improvements test 16 lines 18-20
    
    * improvements test 16 lines 29-32
    
    * improvements test 17 lines 29-32 on, autoscaller off
    
    * remove comments test1
    
    * node exp t1
    
    * node exp t2
    
    * node exp t3
    
    * node exp t4 label 82-84 removed
    
    * node exp t5 selector removed
    
    * node exp t6 final
    
    * prome t1
    
    * prome t2
    
    * prome t3 fin
    
    * prome t4
    
    * prome t5
    
    * port values1
    
    * conf2
    Update build.sh
    
    * conf3
    2 postjob init 1
    
    * comment added
    
    * conflicts1
    
    * conf3
    Conflicts:
    
    * Update build.sh
    
    * chart update
    
    * node-exp fix update
    
    * node-exp fix update1
    
    * node-exp fix update 2
    
    * node-exp fix update 3
    
    * node-exp fix update 4
    
    * node-exp fix update 5
    
    * node-exp fix update 6
    
    * node-exp fix update 7
    
    * node-exp fix update 8
    
    * node-exp fix update 9
    
    * node-exp fix update 10
    
    * small fix1
    
    * landing page update 1
    
    * tf account collector1
    
    * landing page fix1
    
    * fixes in sa and terraform 1
    
    * fixes in sa and terraform 1
    
    * fixes in sa2
    
    * fixes in sa3
    
    * version test1
    
    * test values for sa
    
    * version removed for node exp1
    
    * removed monitoring-sa
    
    * added monitoring-sa in tf1
    
    * renamed gcp sa connection
    
    * sa for test1
    
    * sa for test1
    
    * sa in tf and in monitorint tools fixes1

---
+++ gitops/env/stage2/configs/keycloak/keycloak-post-grafana/keycloak.mjs
@@ -0,0 +1,367 @@
+// Copyright (c) 2024-2025 Accenture, All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import fs from 'fs/promises';
+import _ from 'lodash';
+import KcAdminClient from '@keycloak/keycloak-admin-client';
+import retry from 'async-retry';
+
+const config = {
+  keycloak: {
+    baseUrl: process.env.PLATFORM_URL + '/auth',
+    username: process.env.KEYCLOAK_USERNAME,
+    password: process.env.KEYCLOAK_PASSWORD,
+    realm: {
+      realm: 'horizon'
+    },
+    client: {
+      clientId: 'grafana-oauth',
+      adminUrl: process.env.DOMAIN + '/grafana',
+      redirectUris: [process.env.DOMAIN + '/grafana/login/generic_oauth/*'],
+      protocol: 'openid-connect',
+      publicClient: false
+    },
+    adminUser: {
+      username: process.env.GRAFANA_ADMIN_USERNAME,
+      password: process.env.GRAFANA_ADMIN_PASSWORD,
+      firstName: 'Grafana',
+      lastName: 'Grafana',
+      email: 'grafana@grafana'
+    },
+    clientScope:{
+      clientScopeName: 'groups'
+    },
+    rolesAndGroups: [
+      'horizon-grafana-administrators',
+      'horizon-grafana-viewers'
+    ]
+  }
+};
+
+const keycloakAdmin = new KcAdminClient({
+  baseUrl: config.keycloak.baseUrl
+});
+
+async function waitForKeycloak() {
+  const opts = {
+    retries: 100,
+    minTimeout: 2000,
+    factor: 1,
+    onRetry: (err) => {console.info(`waiting for ${config.keycloak.baseUrl}...`, err.message)}
+  };
+  await retry(login, opts);
+}
+
+async function login()  {
+  try {
+    await keycloakAdmin.auth({
+      'username': config.keycloak.username,
+      'password': config.keycloak.password,
+      'grantType': 'password',
+      'clientId': 'admin-cli'
+    });
+  } catch (err) {
+    throw err
+  }
+}
+
+async function getRealm()  {
+  try {
+    let realm = await keycloakAdmin.realms.findOne({
+      realm: config.keycloak.realm.realm,
+    });
+    keycloakAdmin.setConfig({
+      realmName: realm.realm,
+    });
+    realm.keys = await keycloakAdmin.realms.getKeys({realm: realm.realm});
+    config.keycloak.realm = realm;
+  } catch (err) {
+    throw err
+  }
+}
+
+async function createClientIfRequired()  {
+  try {
+    let clients = await keycloakAdmin.clients.find();
+    let client = _.find(clients, {clientId: config.keycloak.client.clientId});
+    if (client) {
+      console.info('updating %s client', config.keycloak.client.clientId);
+      await keycloakAdmin.clients.update({id: client.id, realm: config.keycloak.realm.realm}, _.merge(client, config.keycloak.client));
+    } else {
+      console.info('creating %s client', config.keycloak.client.clientId);
+      await keycloakAdmin.clients.create(config.keycloak.client);
+    }
+    clients = await keycloakAdmin.clients.find();
+    client = _.find(clients, {clientId: config.keycloak.client.clientId});
+    config.keycloak.client = client;
+  } catch (err) {
+    throw err
+  }
+}
+
+async function createUserIfRequired()  {
+  try {
+    let users = await keycloakAdmin.users.find();
+    let user = _.find(users, {username: config.keycloak.adminUser.username});
+
+    if (user) {
+      console.info('deleting old instance of %s user', config.keycloak.adminUser.username);
+      await keycloakAdmin.users.del({id: user.id});
+    }
+
+    console.info('creating %s user', config.keycloak.adminUser.username);
+    const new_user = await keycloakAdmin.users.create({
+      username: config.keycloak.adminUser.username,
+      enabled: true,
+      requiredActions: [],
+      realm: config.keycloak.realm.realm,
+      firstName: config.keycloak.adminUser.firstName,
+      lastName: config.keycloak.adminUser.lastName,
+      email: config.keycloak.adminUser.email
+    });
+
+  } catch (err) {
+    throw err
+  }
+
+}
+
+async function generateSecretFiles()  {
+  try {
+    let clients = await keycloakAdmin.clients.find();
+    let client = _.find(clients, {clientId: config.keycloak.client.clientId});
+
+    if (client) {
+      console.info('dumping %s client data into json file', config.keycloak.client.clientId);
+      fs.writeFile('client-grafana.json', JSON.stringify(client));
+    }
+  } catch (err) {
+    throw err
+  }
+}
+
+async function addGroupsClientScopeToGrafanaClientIfRequired() {
+  const clientId = config.keycloak.client.clientId;
+  const clientScopeName = config.keycloak.clientScope.clientScopeName;
+
+  try {
+    const clients = await keycloakAdmin.clients.find();
+    const grafanaClient = clients.find(client => client.clientId === clientId);
+
+    if (!grafanaClient) {
+      console.error(`client "${clientId}" does not exist.`);
+      return;
+    }
+
+    const clientScopes = await keycloakAdmin.clientScopes.find();
+    const groupsScope = clientScopes.find(scope => scope.name === clientScopeName);
+
+    if (!groupsScope) {
+      console.error(`client scope "${clientScopeName}" does not exist.`);
+      return;
+    }
+
+    const defaultScopes = await keycloakAdmin.clients.listDefaultClientScopes({ id: grafanaClient.id });
+    const isGroupsScopeAssigned = defaultScopes.some(scope => scope.id === groupsScope.id);
+
+    if (isGroupsScopeAssigned) {
+      console.info('"groups" client scope already exists in "grafana" client.');
+    } else {
+      console.log('adding "groups" client scope to "grafana" client.');
+      await keycloakAdmin.clients.addDefaultClientScope({id: grafanaClient.id, clientScopeId: groupsScope.id,});
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function createGrafanaRealmRolesIfRequired() {
+  const realmRoleNames = config.keycloak.rolesAndGroups;
+
+  for (const realmRoleName of realmRoleNames) {
+    try {
+      let realmRole = await keycloakAdmin.roles.findOneByName({name: realmRoleName});
+      if (realmRole) {
+        console.info(`role ${realmRoleName} exists`);
+      } else {
+        console.log(`creating ${realmRoleName} role`);
+        await keycloakAdmin.roles.create({name: realmRoleName});
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+async function createGrafanaClientRolesIfRequired() {
+  const clientId = config.keycloak.client.clientId;
+  const clientRoleNames = config.keycloak.rolesAndGroups;
+
+  try {
+    const clients = await keycloakAdmin.clients.find();
+    const grafanaClient = clients.find(client => client.clientId === clientId);
+
+    if (!grafanaClient) {
+      console.error(`client "${clientId}" does not exist.`);
+      return;
+    }
+
+    const existingRoles = await keycloakAdmin.clients.listRoles({ id: grafanaClient.id });
+
+    for (const roleName of clientRoleNames) {
+      const roleExists = existingRoles.some(role => role.name === roleName);
+      if (roleExists) {
+        console.info(`client role "${roleName}" already exists for "${clientId}".`);
+        continue;
+      }
+
+      await keycloakAdmin.clients.createRole({id: grafanaClient.id, name: roleName});
+      console.log(`client role "${roleName}" created for client "${clientId}".`);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function createGrafanaRealmGroupsIfRequired() {
+  const realmGroupNames = config.keycloak.rolesAndGroups;
+
+  for (const realmGroupName of realmGroupNames) {
+    try {
+      const existingGroups = await keycloakAdmin.groups.find({ search: realmGroupName });
+      const matchedGroup = existingGroups.find(group => group.name === realmGroupName);
+
+      if (matchedGroup) {
+        console.info(`group "${realmGroupName}" already exists.`);
+      } else {
+        console.log(`creating group "${realmGroupName}".`);
+        await keycloakAdmin.groups.create({ name: realmGroupName });
+      }
+    } catch (err) {
+      throw err;
+    }
+  }
+}
+
+async function mapGrafanaRealmRolesIntoClientRolesIfRequired() {
+  const clientId = config.keycloak.client.clientId;
+  const roleNames = config.keycloak.rolesAndGroups;
+
+  try {
+    const clients = await keycloakAdmin.clients.find();
+    const grafanaClient = clients.find(client => client.clientId === clientId);
+
+    if (!grafanaClient) {
+      console.error(`client "${clientId}" does not exist.`);
+      return;
+    }
+
+    for (const roleName of roleNames) {
+      const clientRole = await keycloakAdmin.clients.findRole({id: grafanaClient.id, roleName});
+
+      if (!clientRole) {
+        console.warn(`client role "${roleName}" does not exist under client "${clientId}".`);
+        continue;
+      }
+
+      const realmRole = await keycloakAdmin.roles.findOneByName({ name: roleName });
+      if (!realmRole) {
+        console.warn(`realm role "${roleName}" does not exist.`);
+        continue;
+      }
+
+      let parentRole = await keycloakAdmin.clients.findRole({id: grafanaClient.id, roleName: roleName});
+      let childRole = await keycloakAdmin.roles.findOneByName({name: roleName});
+      await keycloakAdmin.roles.createComposite({roleId: parentRole.id}, [childRole]);
+      console.log(`realm role "${roleName}" mapped into client role "${roleName}".`);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function mapGrafanaClientRolesToGroupsIfRequired() {
+  const clientId = config.keycloak.client.clientId;
+  const roleGroupNames = config.keycloak.rolesAndGroups;
+
+  try {
+    const clients = await keycloakAdmin.clients.find();
+    const grafanaClient = clients.find(client => client.clientId === clientId);
+
+    if (!grafanaClient) {
+      console.error(`client "${clientId}" does not exist.`);
+      return;
+    }
+
+    for (const roleGroupName of roleGroupNames) {
+      const clientRole = await keycloakAdmin.clients.findRole({id: grafanaClient.id, roleName: roleGroupName});
+
+      if (!clientRole) {
+        console.warn(`client role "${roleGroupName}" does not exist in "${clientId}".`);
+        continue;
+      }
+
+      const allGroups = await keycloakAdmin.groups.find();
+      const group = allGroups.find(g => g.name === roleGroupName);
+
+      if (!group) {
+        console.warn(`group "${roleGroupName}" does not exist.`);
+        continue;
+      }
+
+      const mappedRoles = await keycloakAdmin.groups.listClientRoleMappings({id: group.id, clientUniqueId: grafanaClient.id});
+      const alreadyMapped = mappedRoles.some(role => role.name === clientRole.name);
+
+      if (alreadyMapped) {
+        console.info(`client role "${roleGroupName}" is already mapped to group "${roleGroupName}".`);
+        continue;
+      }
+
+      await keycloakAdmin.groups.addClientRoleMappings({
+        id: group.id,
+        clientUniqueId: grafanaClient.id,
+        roles: [{
+          id: clientRole.id,
+          name: clientRole.name
+        }]
+      });
+      console.log(`client role "${roleGroupName}" mapped to group "${roleGroupName}".`);
+    }
+  } catch (err) {
+    throw err;
+  }
+}
+
+async function configureKeycloak()  {
+  try {
+    await waitForKeycloak();
+    await getRealm();
+    await createClientIfRequired();
+    await createUserIfRequired();
+    await generateSecretFiles();
+    await addGroupsClientScopeToGrafanaClientIfRequired();
+    await createGrafanaRealmRolesIfRequired();
+    await createGrafanaRealmGroupsIfRequired();
+    await createGrafanaClientRolesIfRequired();
+    await mapGrafanaRealmRolesIntoClientRolesIfRequired();
+    await mapGrafanaClientRolesToGroupsIfRequired();
+  } catch (err) {
+    throw err
+  }
+}
+
+configureKeycloak()
+  .catch((err) => {
+    console.error(err.message);
+  });

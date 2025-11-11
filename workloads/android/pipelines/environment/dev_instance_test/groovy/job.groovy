147258f7d3dabfbc0cca20c622c185e373071073
David Smith <dave.m.smith@accenture.com>
2025-11-11 21:17:44 +0000

    TAA-1238: Update cuttlefish to v1.31.0
    v1.30.0 was stable but now marked unstable, hence update.

--- workloads/android/pipelines/environment/dev_instance_test/groovy/job.groovy
+++ workloads/android/pipelines/environment/dev_instance_test/groovy/job.groovy
@@ -28,9 +28,9 @@ pipelineJob('Android/Environment/Development Test Instance') {
       description('''<p>The Jenkins GCE Clouds label for the VM instance template, e.g.<br/></p>
         <ul>
           <li>cuttlefish-vm-main</li>
-          <li>cuttlefish-vm-v1300</li>
+          <li>cuttlefish-vm-v1310</li>
           <li>cuttlefish-vm-main-arm64</li>
-          <li>cuttlefish-vm-v1300-arm64</li>
+          <li>cuttlefish-vm-v1310-arm64</li>
         </ul>''')
       trim(true)
     }
--- workloads/android/pipelines/tests/cts_execution/groovy/job.groovy
+++ workloads/android/pipelines/tests/cts_execution/groovy/job.groovy
@@ -44,9 +44,9 @@ href="https://source.android.com/docs/compatibility/cts/downloads" target="_blan
       description('''<p>The Jenkins GCE Clouds label for the Cuttlefish instance template, e.g.<br/></p>
         <ul>
           <li>cuttlefish-vm-main</li>
-          <li>cuttlefish-vm-v1300</li>
+          <li>cuttlefish-vm-v1310</li>
           <li>cuttlefish-vm-main-arm64</li>
-          <li>cuttlefish-vm-v1300-arm64</li>
+          <li>cuttlefish-vm-v1310-arm64</li>
         </ul>''')
       trim(true)
     }

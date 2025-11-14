e8d89b1d8df8fa930a5acc71096c219e4dface9e
David Smith <dave.m.smith@accenture.com>
2025-11-14 12:09:22 +0000

    TAA-1247: Fix shebangs

--- workloads/cloud-workstations/pipelines/workstation-images/horizon-android-studio/assets/etc/workstation-startup.d/230_google-chrome-default.sh
+++ workloads/cloud-workstations/pipelines/workstation-images/horizon-android-studio/assets/etc/workstation-startup.d/230_google-chrome-default.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 #
 # Copyright 2024 Google Inc. All Rights Reserved.
 #
--- workloads/cloud-workstations/pipelines/workstation-images/horizon-asfp/assets/google/scripts/build_vcar.sh
+++ workloads/cloud-workstations/pipelines/workstation-images/horizon-asfp/assets/google/scripts/build_vcar.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Copyright (c) 2024-2025 Accenture, All Rights Reserved.
 #

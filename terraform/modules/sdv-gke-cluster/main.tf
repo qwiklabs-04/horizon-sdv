6e1fd436ccb3766362f7f4f6dfb5ef84affa86e4
David Smith <dave.m.smith@accenture.com>
2025-12-05 16:41:18 +0000

    TEST lb toggle

--- terraform/modules/sdv-gke-cluster/main.tf
+++ terraform/modules/sdv-gke-cluster/main.tf
@@ -80,7 +80,7 @@ resource "google_container_cluster" "sdv_cluster" {
 
   addons_config {
     http_load_balancing {
-      disabled = false
+      disabled = true
     }
     gcp_filestore_csi_driver_config {
       enabled = true
